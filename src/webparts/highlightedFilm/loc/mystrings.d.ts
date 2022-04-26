9963065032255feaa27738aa6f5192a810e0213e
s.boutier <s.boutier@LGOP4422>
2022-04-26 10:14:31 +0200

    first commit

---
+++ src/webparts/highlightedFilm/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IHighlightedFilmWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'HighlightedFilmWebPartStrings' {
+  const strings: IHighlightedFilmWebPartStrings;
+  export = strings;
+}
